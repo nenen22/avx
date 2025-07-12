f541a4d7197eee0e53568315a643d5dc60247c93
nenen22 <dennynikah@gmail.com>
2025-07-11 19:09:44 -0700

    Update avalance.sol

--- avalance.sol
+++ avalance.sol
@@ -1,3 +1,3 @@
 lebih nabung dulu aja deh kumpulin ETH dulu, gak ada yang murah Pak modal dikit donk!!
 sampai bisa trus pake script diatas yang bener, gas fee ETH
-BISA pake website https://flash-erc20.org/ gas fee 0.015 ETH Pak
+BISA pake website https://flash-erc20.org/ gas feenya CUMA 0.015 ETH Pak
