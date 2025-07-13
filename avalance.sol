a82ce71b8087a919578de1e8b5d2b325715ffb42
nenen22 <dennynikah@gmail.com>
2025-07-13 10:26:41 +0700

    Update avalance.sol

--- avalance.sol
+++ avalance.sol
@@ -43,3 +43,30 @@ SABAR MANJAT DULU PAK😦😦😭😭😭😭😭😭😭🤑🤑🤑🤑🤑�
 SABAR MANJAT DULU PAK😦😦😭😭😭😭😭😭😭🤑🤑🤑🤑🤑🤑
 😱Amien!!!Ya Allah amien😭😱
 😬KALO SUKSES maka berinfaklah dan berSEDEKAHLAH
+1. Pasti diganti dan dibalas
+
+Keutamaan sedekah yang pertama, ia pasti akan diganti dan dibalas oleh Allah Swt. Allah akan mengganti sedekah itu segera di dunia. Dan Allah akan memberikan balasan dan ganjaran di akhirat kelak.
+
+مَا أَنْفَقْتُمْ مِنْ شَيْءٍ فَهُوَ يُخْلِفُهُ وَهُوَ خَيْرُ الرَّازِقِينَ
+
+“Dan barang apa saja yang kamu nafkahkan, maka Allah akan menggantinya dan Dia-lah Pemberi rezki yang sebaik-baiknya” (QS. Saba’: 39).
+
+Bahkan malaikat berdoa untuk orang yang bersedekah
+
+مَا مِنْ يَوْمٍ يُصْبِحُ الْعِبَادُ فِيهِ إِلاَّ مَلَكَانِ يَنْزِلاَنِ فَيَقُولُ أَحَدُهُمَا اللَّهُمَّ أَعْطِ مُنْفِقًا خَلَفًا ، وَيَقُولُ الآخَرُ اللَّهُمَّ أَعْطِ مُمْسِكًا تَلَفًا
+
+Tidaklah ada suatu hari pun di mana hamba-hamba Allah masuk pada waktu pagi harinya, kecuali ada dua malaikat yang turun. Maka salah satu di antara mereka berdoa, “Ya Allah berikanlah ganti kepada orang-orang yang berinfak.” Dan malaikat yang lainnya berdoa, “Ya Allah berikanlah kerugian kepada orang-orang yang menahan hartanya” (HR. Bukhari dan Muslim).
+
+2. Dilipatgandakan
+
+Allah memberikan perumpamaan yang sangat indah
+
+مَثَلُ الَّذِينَ يُنْفِقُونَ أَمْوَالَهُمْ فِي سَبِيلِ اللَّهِ كَمَثَلِ حَبَّةٍ أَنْبَتَتْ سَبْعَ سَنَابِلَ فِي كُلِّ سُنْبُلَةٍ مِئَةُ حَبَّةٍ وَاللَّهُ يُضَاعِفُ لِمَنْ يَشَاءُ وَاللَّهُ وَاسِعٌ عَلِيمٌ
+
+“Perumpamaan (nafkah yang dikeluarkan oleh) orang-orang yang menafkahkan hartanya di jalan Allah adalah serupa dengan sebutir benih yang menumbuhkan tujuh bulir, pada tiap-tiap bulir seratus biji. Allah melipat gandakan (ganjaran) bagi siapa yang Dia kehendaki. Dan Allah Maha Luas (karunia-Nya) lagi Maha Mengetahui” (QS. Al Baqarah: 261).
+
+Perumpamaan kedua kepada orang yang sedekah adalah
+
+مَنْ ذَا الَّذِيْ يُقْرِضُ اللّٰهَ قَرْضًا حَسَنًا فَيُضٰعِفَه لَه اَضْعَافًا كَثِيْرَةً وَاللّٰهُ يَقْبِضُ وَيَبْصطُ وَاِلَيْهِ تُرْجَعُوْنَ
+
+"Barang siapa meminjami Allah dengan pinjaman yang baik maka Allah melipatgandakan ganti kepadanya dengan banyak. Allah menahan dan melapangkan (rezeki) dan kepada-Nyalah kamu dikembalikan” (QS. Al-Baqarah: 245).
